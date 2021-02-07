d32f5e39b0576a63fbd73848d44039cd0a339c06
uhureis <68116848+uhureis@users.noreply.github.com>
2021-02-07 14:04:42 +0300

    07.02.2021

---
+++ HackerRank/AppleAndOrange/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace AppleAndOrange
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            int[] apples = new int[] { -2, 2, 1 };
+            int[] oranges = new int[] { 5, -6 };
+            CountApplesAndOranges(7, 11, 5, 15, apples, oranges);
+            Console.ReadLine();
+        }
+
+        private static void CountApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges)
+        {
+            Apple(s, t, a, apples);
+            Orange(s, t, b, oranges);
+        }
+
+        private static void Apple(int s, int t, int a, int[] apples)
+        {
+            int sum = 0;
+            for (int i = 0; i < apples.Length; i++)
+            {
+                if (apples[i] + a >= s && apples[i] + a <= t) sum++;
+            }
+            Console.WriteLine(sum);
+        }
+
+        private static void Orange(int s, int t, int b, int[] oranges)
+        {
+            int sum = 0;
+            for (int i = 0; i < oranges.Length; i++)
+            {
+                if (oranges[i] + b <= t && oranges[i] + b >= s) sum++;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ HackerRank/GradingStudents/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradingStudents
+{
+    internal class Program
+    {
+        private static void Main() // 1
+        {
+            List<int> grades = new List<int>() { 73, 67, 38, 33 };
+            List<int> newGrades = GradingStudents(grades);
+            foreach (var item in newGrades)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+
+        public static List<int> GradingStudents(List<int> grades) // 2
+        {
+            List<int> CurrentScore = new List<int>() { };
+            ModBase(grades, CurrentScore);
+            return CurrentScore;
+        }
+
+        private static void ModBase(List<int> grades, List<int> CurrentScore) // 2.1
+        {
+            for (int i = 0; i < grades.Count; i++)
+            {
+                int points = grades[i];
+                Mod(points, CurrentScore);
+            }
+        }
+
+        private static void Mod(int point, List<int> CurrentScore) // 2.2
+        {
+            if (point > 37) ModConditions(point, CurrentScore);
+            else CurrentScore.Add(point);
+        }
+
+        private static void ModConditions(int point, List<int> CurrentScore)// 2.3
+        {
+            int value = point % 5;
+            if (value == 0) CurrentScore.Add(point);
+            else LastCheck(value, point, CurrentScore);
+        }
+
+        private static void LastCheck(int value, int point, List<int> CurrentScore)// 2.4
+        {
+            int interval = 5 - value;
+            if (interval < 3) CurrentScore.Add(point + interval);
+            else CurrentScore.Add(point);
+        }
+    }
+}
