c6414e59accb4f93468452956d7a3a34ebf50b77
uhureis <68116848+uhureis@users.noreply.github.com>
2021-02-11 22:15:06 +0300

    New Edit.

---
+++ HackerRank/BillDivision/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace BillDivision
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            BonAppetit(new List<int>() { 3, 10, 2, 9 }, 1, 12);
+        }
+
+        // k listede hangi yemeği yemediği , b anna ne kadar ödedi
+        private static void BonAppetit(List<int> bill, int k, int b)
+        {
+            int mustBe;
+            int a = 0;
+            int unpaid = bill[k];
+            var newBill = bill.ToList();
+            string result;
+
+            newBill.Remove(unpaid);
+            newBill.ForEach(delegate (int x) { a += x; });
+            mustBe = a / 2;
+            result = (mustBe == b) ? "Bon Appetit" : (b - mustBe).ToString();
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
