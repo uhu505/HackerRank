1bff171f8089ca6d9a6302a0266390b5dab6f276
uhureis <68116848+uhureis@users.noreply.github.com>
2021-02-12 15:41:46 +0300

    New Edit, New Question.

---
+++ HackerRank/DrawingBook/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DrawingBook
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            int result = PageCount(6, 5);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        private static int PageCount(int n, int p)
+        {
+            int Front = n / 2; //3
+            int FrontTurnCount = p / 2; //3
+            int BackTurnCount = Front - FrontTurnCount;
+            return Math.Min(FrontTurnCount, BackTurnCount);
+        }
+    }
+}
